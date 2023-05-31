51092c64d1ef461a864da622d401f6003d4f1c2c
Olaf Mihari <olafnjenga@gmail.com>
2023-05-31 09:16:40 +0300

    completed assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,15 @@
+# db/seeds.rb
+
 # Add a console message so we can see output when the seed file runs
+puts "Seeding games table..."
+
+50.times do
+  Game.create(
+    title: Faker::Game.title,
+    platform: Faker::Game.platform,
+    genre: Faker::Game.genre,
+    price: Faker::Number.between(from: 0, to: 100)
+  )
+end
+
+puts "Seeding completed!"
